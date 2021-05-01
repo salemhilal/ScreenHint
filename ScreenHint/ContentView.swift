8d915b78424de3bfb1c1da74f6a82cff298e28ec
Salem Hilal <salemhilal@gmail.com>
2021-05-01 00:33:18 -0400

    Initial Commit

---
+++ ScreenHint/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScreenHint
+//
+//  Created by Salem on 5/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScreenHint/ScreenHintApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenHintApp.swift
+//  ScreenHint
+//
+//  Created by Salem on 5/1/21.
+//
+
+import SwiftUI
+
+@main
+struct ScreenHintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
