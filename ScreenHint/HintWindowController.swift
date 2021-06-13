26472fefd1b31e6e86447f1ebc79f44943a4b0ae
Salem Hilal <salemhilal@gmail.com>
2021-06-13 10:33:39 -0400

    Bugfixes, prompt for permissions
    A few bugfixes
      - The shortcut is Cmd + Shift + 2, not Cmd + Shift + 1
      - We should prompt for permissions, rather than waiting until we take a screenshot.
      - We shouldn't take a screenshot without the permissions to take a screenshot.
    
    Also, I moved HintWindowController and HintWindow into a separate file. More refactoring to come.

---
+++ ScreenHint/HintWindowController.swift
@@ -0,0 +1,186 @@
+//
+//  HintWindowController.swift
+//  ScreenHint
+//
+//  Created by Salem on 6/13/21.
+//
+
+import Foundation
+import AppKit
+
+
+protocol CopyDelegate {
+    func shouldCopy();
+}
+
+
+/**
+ This is a window that closes when you doubleclick it.
+ */
+class HintWindow: NSWindow {
+    
+    var copyDelegate: CopyDelegate?
+    var screenshot: CGImage? = nil
+    
+    override var canBecomeKey: Bool {
+        get {
+            return true
+        }
+    }
+    
+    override var canBecomeMain: Bool {
+        get {
+            return false
+        }
+    }
+    
+    override func keyDown(with event: NSEvent) {
+        if (event.charactersIgnoringModifiers == "c" && event.modifierFlags.contains(.command)) {
+            self.copyDelegate?.shouldCopy()
+        }
+    }
+    
+    override func mouseUp(with event: NSEvent) {
+        // If this window is double-clicked (anywhere), close it.
+        if event.clickCount >= 2 {
+            self.orderOut(self)
+            self.windowController?.dismissController(nil)
+            // TODO: remove self from rects array
+        }
+        super.mouseUp(with: event)
+    }
+}
+
+class HintWindowController:  NSWindowController, NSWindowDelegate, CopyDelegate, NSMenuDelegate {
+    var allDesktopsMenuItem: NSMenuItem?
+    func shouldCopy() {
+        guard let screenshot = self.screenshot else {
+            return
+        }
+        
+        let image = NSImage.init(cgImage: screenshot, size: self.window!.frame.size)
+        NSPasteboard.general.clearContents()
+        NSPasteboard.general.writeObjects([image])
+    }
+    
+    @objc func menuCopyHandler(_ sender: AnyObject?) {
+        self.shouldCopy()
+    }
+    
+    @objc func menuShowOnAllDesktopsHandler(_ sender: AnyObject?) {
+        let oldState = self.allDesktopsMenuItem?.state;
+        let newState: NSControl.StateValue = oldState == .on ? .off : .on;
+        self.allDesktopsMenuItem?.state = newState;
+        
+        if (newState == .on) {
+            self.window?.collectionBehavior = [.canJoinAllSpaces]
+        } else {
+            self.window?.collectionBehavior = [.managed]
+        }
+    }
+    
+    var screenshot: CGImage?
+        
+    init(_ rect: NSRect) {
+        // TODO: when the window's origin changes (i.e. when dragging from top-right
+        //       to bottom-left), the window jitters sliglty.
+        let window = HintWindow(contentRect: rect, styleMask: [.resizable], backing: .buffered, defer: false)
+        window.isOpaque = false
+        window.level = .screenSaver
+        window.backgroundColor = NSColor.blue
+        window.alphaValue = 0.2
+        window.ignoresMouseEvents = false
+        window.isMovableByWindowBackground = true
+        window.isMovable = true
+        window.hasShadow = true
+        window.contentView?.wantsLayer = true
+        window.contentView?.layer?.borderWidth = 1
+        window.contentView?.layer?.borderColor = CGColor.black
+        // Causes a fast fade-out (at least at time of writing)
+        window.animationBehavior = .utilityWindow
+        
+        // TODO: Make this configurable
+        window.collectionBehavior = [.canJoinAllSpaces]
+
+        super.init(window: window)
+        
+        window.delegate = self
+        window.copyDelegate = self
+        let menu = NSMenu()
+        
+        let copyItem = menu.addItem(withTitle: "Copy", action:#selector(self.menuCopyHandler(_:)), keyEquivalent: "")
+        copyItem.keyEquivalentModifierMask = [.command]
+        copyItem.target = self
+        
+        
+        let showItem = menu.addItem(withTitle: "Show On All Desktops", action:#selector(self.menuShowOnAllDesktopsHandler(_:)),
+                                                keyEquivalent: "")
+        showItem.state = .on
+        showItem.target = self
+        self.allDesktopsMenuItem = showItem
+        
+        window.menu = menu
+    }
+    
+    override func rightMouseUp(with event: NSEvent) {
+        let point = NSEvent.mouseLocation;
+        self.window!.menu?.popUp(positioning: nil, at: point, in: nil)
+    }
+    
+    func setRect(_ rect: NSRect) {
+        self.window?.setFrame(rect, display: true, animate: false)
+    }
+    
+    func finishDragging() {
+        // "The origin point of a rectangle is at its bottom left in Quartz/Cocoa on OS X."
+        // but that's not true for the rect passed to CGWindowListCreateImage
+        // https://stackoverflow.com/a/12438416/444912
+        guard let window = self.window else {
+            return;
+        }
+        
+        // omg this was so dumb but the main screen is NOT the first screen.
+        // the coordinates are all relative to the _first_ screen. jesus f christ lol
+        // TODO: this, in its own function, with tests.
+        let screen = NSScreen.screens[0];
+        let screenHeight = screen.frame.height
+        let windowRect = window.frame;
+        let screenshotRect = NSRect(
+            x: windowRect.minX,
+            y: screenHeight - windowRect.minY - windowRect.height,
+            width: windowRect.width,
+            height: windowRect.height)
+        
+        
+        // Make sure the window keeps its aspect ratio when resizing
+        window.aspectRatio = window.frame.size
+        window.alphaValue = 0.0
+        window.backgroundColor = NSColor.clear
+        
+        // Wait a split second for the hint's blue background to be removed
+        // before taking the screenshot
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            let screenshot = CGWindowListCreateImage(screenshotRect,
+                                                     CGWindowListOption.optionAll,
+                                                     kCGNullWindowID,
+                                                     CGWindowImageOption.bestResolution)!
+            
+            let image = NSImage(cgImage:screenshot, size: .zero)
+            let imageView = WindowDraggableImageView(frame: NSRect(origin: .zero, size: self.window!.frame.size))
+            imageView.image = image
+            // Make sure the image fills the window
+            imageView.autoresizingMask = [.height, .width]
+            
+            self.window?.alphaValue = 1.0
+            self.window?.level = .floating
+            self.window?.isOpaque = true
+            self.window?.contentView?.addSubview(imageView)
+            self.screenshot = screenshot
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
